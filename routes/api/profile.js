d4586848ab2911ada4e0cf9fee23a4eb4280abe4
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-06 14:21:41 -0400

    Updated deprecation warnings in the mongoose db connect. Created the profile route to allow for adding and updating the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
 const auth = require("../../middleware/auth");
+// const { check, validationResult } = require("express-validator/check"); // Deprecated
+// still can use the check method with express-validator
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -24,5 +28,90 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route    POST api/profile
+// @desc     Create or update a user profile
+// @access   Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // If there are errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Destructuring/retrive everything from the req.body
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body;
+
+    // Build profile object to insert into Db and add the profileFields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    // Check if fields are coming in
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // Build/Initialize social object
+    profileFields.social = {};
+    // Check if fields are coming in
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    // Look for the profile by the user
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      // if found
+      if (profile) {
+        // Update it
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        // And return it
+        return res.json(profile);
+      }
+      // If not, Create a new one
+      profile = new Profile(profileFields);
+
+      // Save and return the new profile
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.massage);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
