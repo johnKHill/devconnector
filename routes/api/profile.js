410a2b592c64d1bc20f956f75e57af17d0f53d49
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-08 12:16:19 -0400

    Added Axios to assist in my async and await function to retrieve github accounts. Created a github access token for OAUTH in my config file to add to my headers objects in order to use the github api.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const axios = require("axios");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 
 const auth = require("../../middleware/auth");
@@ -345,6 +348,28 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from github
+// @access   Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubToken")}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+
+    res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).send({ msg: "No Github profile found" });
   }
 });
 module.exports = router;
