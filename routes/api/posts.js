f97e9d869643bdcb6353d2e9b5090318305f33a8
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-08 15:09:17 -0400

    Create a new Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,61 +1,61 @@
-// const express = require("express");
-// const router = express.Router();
-// const auth = require("../../middleware/auth");
-// // MODElS
-// const Post = require("../../models/Post");
-// const Profile = require("../../models/Profile");
-// const User = require("../../models/User");
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+// MODElS
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// // const { check, validationResult } = require("express-validator/check"); // Deprecated
-// // still can use the check method with express-validator
-// const { check, validationResult } = require("express-validator");
+// const { check, validationResult } = require("express-validator/check"); // Deprecated
+// still can use the check method with express-validator
+const { check, validationResult } = require("express-validator");
 
-// // @route    POST api/posts
-// // @desc     Create a post
-// // @access   Private
-// router.post(
-//   "/",
-//   [
-//     auth,
-//     [
-//       check("text", "Text is required")
-//         .not()
-//         .isEmpty()
-//     ]
-//   ],
-//   async (req, res) => {
-//     // Checking for errors in the body
-//     const errors = validationResult(req);
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    // Checking for errors in the body
+    const errors = validationResult(req);
 
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-//     try {
-//       // get the user
-//       const user = await User.findById(req.user.id).select("-password");
+    try {
+      // get the user
+      const user = await User.findById(req.user.id).select("-password");
 
-//       // Create a new post object
-//       const newPost = new Post ({
-//         text: req.body.text,
-//         name: user.name,
-//         avatar: user.avatar,
-//         user: req.user.id
-//       });
+      // Create a new post object
+      const newPost = new Post ({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
 
-//       // Save the new promised post to the DB
-//       const post = await newPost.save();
+      // Save the new promised post to the DB
+      const post = await newPost.save();
 
-//       // Next, get the post back in the response
-//       res.json(post)
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send("Server Error")
-//     }
+      // Next, get the post back in the response
+      res.json(post)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error")
+    }
 
 
 
-//   }
-// );
+  }
+);
 
-// module.exports = router;
+module.exports = router;
