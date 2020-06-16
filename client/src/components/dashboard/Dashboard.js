1af49c0cb6cf1bbd12a88151b1ee467b3e2cfc4c
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-16 14:56:51 -0400

    Added the useState hook to the Create Profile component to assist in the toggle of the social media links. Also, I've addded the profile form template I've created into the component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -28,7 +28,7 @@ const Dashboard = ({
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/crate-profile" className="btn btn-primary my-1">
+          <Link to="/create-profile" className="btn btn-primary my-1">
             Create Profile
           </Link>
         </Fragment>
