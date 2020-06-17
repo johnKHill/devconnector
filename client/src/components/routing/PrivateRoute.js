1f9ec4229576acb210b630b9762389bc99661cc0
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-17 15:37:51 -0400

    Profiles component setup to loop and display the saved profiles

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+
 const PrivateRoute = ({
   component: Component,
   auth: { isAuthenticated, loading },
