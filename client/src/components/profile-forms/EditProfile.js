4af81f1e3c60c4d35c7ba583d33b158c2d90fd67
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-06-18 20:02:37 -0400

    Removed profile from the setFormDate

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -45,7 +45,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? "" : profile.social.youtube,
       instagram: loading || !profile.social ? "" : profile.social.instagram,
     });
-  }, [loading, profile, getCurrentProfile]);
+  }, [loading, getCurrentProfile]);
 
   const {
     company,
